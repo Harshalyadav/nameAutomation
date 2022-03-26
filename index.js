28b80d059b5e4bd879d13d87f86fb25514510b4b
Harshalyadav <shailyadav3166@gmail.com>
2022-03-26 12:50:58 +0530

    add color

--- index.js
+++ index.js
@@ -3,22 +3,27 @@
 const clearCon = require('clear-any-console');
 const pacJSON = require('./package.json');
 
+const chalk = require('chalk');
+
 clearCon();
 
 console.log(`
+   ${pacJSON.description}
 
- Hello hey!
- My name is Harshal yadav.
- I'm a full stack web developer using MERN.
- I'v completed my graduation in 2021.
+   Hello  ${chalk.red('!!!!')}
+
+      My name is ${chalk.hex("#4aee88").bold.inverse(' Harshal yadav ')}.
+      I'm a full stack web developer using MERN.
+      I'v completed my graduation in ${chalk.hex('#3e22a6').italic('2021')} .
  
  
- github :  https://github.com/Harshalyadav
- 
+${chalk.hex('#1da1f2').bold.inverse(' linked in ')} ${chalk.dim('https://linkedin.com/Harshalyadav')}
+${chalk.hex('#6cc644').bold.inverse(' github    ')} ${chalk.dim('https://github.com/Harshalyadav')}
+  
  `);
  
+
  
  console.log(`
- version : ${pacJSON.version}
- 
- `);
+${chalk.hex("#f1279d").bold.inverse(' version ')} : ${pacJSON.version}
+`);
